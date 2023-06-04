c8a2706a3e9538af23f4b68b8bbca99963365322
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2023-06-04 23:28:23 +0700

    please be final

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,7 +1,5 @@
 import admin from 'firebase-admin';
-import dotenv from 'dotenv';
-dotenv.config();
-const serviceAccount = JSON.parse(process.env.SECRET);
+import serviceAccount from './secret.json';
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
