1018cb2a586e874704dd2164057ead91932bbbd1
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2023-06-05 00:16:35 +0700

    edit cloudbuild.yaml again

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,16 +1,10 @@
 import admin from 'firebase-admin';
-import { SecretManagerServiceClient } from '@google-cloud/secret-manager';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// Create an instance of the Secret Manager service client
-const secretClient = new SecretManagerServiceClient();
 
-// Fetch the secret value from Secret Manager
-const [version] = await secretClient.accessSecretVersion({
-  name: 'projects/587913373658/secrets/FIREBASE_SECRET/versions/1',
-});
-const serviceAccount = JSON.parse(version.payload.data.toString());
+const serviceAccount = JSON.parse(process.env.SECRET_JSON);
 
-// Initialize the Firebase Admin SDK
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
