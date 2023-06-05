c3335c2caa2c2d23973d069712d21624328c6034
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2023-06-05 13:50:56 +0700

    changing approach

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,12 +1,22 @@
 import admin from 'firebase-admin';
-import dotenv from 'dotenv';
-dotenv.config();
 
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email:process.env.EMAIL,
+  client_id:process.env.CLIENT_ID,
+  auth_uri:process.env.AUTH_URI,
+  token_uri:process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROV,
+  client_x509_cert_url:process.env.CLIENT_CERT,
+  universe_domain:process.env.DOMAIN
 
-const serviceAccount = JSON.parse(process.env.SECRET_JSON);
+}
 
 const app = admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(JSON.parse(JSON.stringify(serviceAccount))),
 });
 
 export default app;
