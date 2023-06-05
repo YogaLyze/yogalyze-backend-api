8ef2ff0e2a529bdbd4853b583c303f9fc7f0698d
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2023-06-05 15:11:37 +0700

    delete old files

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,20 +1,7 @@
 import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
-// const serviceAccount = {
-//   type: process.env.TYPE,
-//   project_id: process.env.PROJECT_ID,
-//   private_key_id: process.env.PRIVATE_KEY_ID,
-//   private_key: process.env.PRIVATE_KEY,
-//   client_email:process.env.EMAIL,
-//   client_id:process.env.CLIENT_ID,
-//   auth_uri:process.env.AUTH_URI,
-//   token_uri:process.env.TOKEN_URI,
-//   auth_provider_x509_cert_url: process.env.AUTH_PROV,
-//   client_x509_cert_url:process.env.CLIENT_CERT,
-//   universe_domain:process.env.DOMAIN
 
-// };
 console.log("PORT: ", process.env.PORT)
 console.log("SECRET: ", process.env.SECRET);
 
