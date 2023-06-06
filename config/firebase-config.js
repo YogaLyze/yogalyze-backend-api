97aafb273c36977b7187bbdbe6697cd5bc063d43
naufalabdillah1908 <84857455+naufalabdillah1908@users.noreply.github.com>
2023-06-06 20:26:54 +0700

    delete testing and dependencies

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -2,11 +2,6 @@ import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
-console.log("PORT: ", process.env.PORT)
-console.log("SECRET: ", process.env.SECRET);
-
-
-
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(JSON.parse(process.env.SECRET)),
